7958c135b091265e7757e1841ea6bf064dce8998
davidkhachatryan123 <davidkhachatryan359@gmail.com>
2026-06-09 19:45:12 +0400

    chore: micro fixes

--- samples/DavidGroup.Core.Essentials.CompleteSample/Extensions/Extensions.cs
+++ samples/DavidGroup.Core.Essentials.CompleteSample/Extensions/Extensions.cs
@@ -5,19 +5,22 @@ namespace DavidGroup.Core.Essentials.CompleteSample.Extensions;
 
 public static class Extensions
 {
-    public static IServiceCollection AddRepositories(this IServiceCollection services)
+    extension(IServiceCollection services)
     {
-        services.AddScoped<IProductsRepository, ProductsRepository>();
-        services.AddScoped<IManufacturersRepository, ManufacturersRepository>();
+        public IServiceCollection AddRepositories()
+        {
+            services.AddScoped<IProductsRepository, ProductsRepository>();
+            services.AddScoped<IManufacturersRepository, ManufacturersRepository>();
 
-        return services;
-    }
+            return services;
+        }
 
-    public static IServiceCollection AddServices(this IServiceCollection services)
-    {
-        services.AddScoped<IProductsService, ProductsService>();
-        services.AddScoped<IManufacturersService, ManufacturersService>();
+        public IServiceCollection AddServices()
+        {
+            services.AddScoped<IProductsService, ProductsService>();
+            services.AddScoped<IManufacturersService, ManufacturersService>();
 
-        return services;
+            return services;
+        }
     }
 }
